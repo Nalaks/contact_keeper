1f2a5e3915640df655641a506d1d6741a257fa56
Stefan <insertinsight@gmail.com>
2020-08-17 20:10:23 +0200

    mongo db setup

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 // connect database
 connectDB();
 
+// middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Thunderdome!' }));
 
 // app routes
